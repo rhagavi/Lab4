49b1ade14b85a92b156ec89c88ee8524339388e3
Thiagarajah <rthiagar@UMICH.EDU>
2019-02-07 15:40:35 -0500

    Change2

--- duck.java
+++ duck.java
@@ -1,25 +1,4 @@
-Public class duck {
-          /*
-           * Master branch is responsible for fly() method.
-           */
-          public fly()
-                  System.out.println("A duck can fly");
-          }
-
-          /*
-           * Tom branch is responsible for swim() method.
-           */
-           public swim(){
-<<<<<<< HEAD
-                 system.out.println("A duck can swim");
-=======
-                 system.out.println("A duck can swim");
-         
-          /*
-           * Bob branch is responsible for eat() method.
-           */
-           public eat(){
-                  system.out.println("A duck can eat");
-           }
-    }
->>>>>>> Bob
+Public class duck{
+ //hiedhe\
+ System.out.print("Hello Duck");
+}
